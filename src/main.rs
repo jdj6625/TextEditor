7b5a370418a34f7d591e62c36af936a4bc47627a
JONATHAN_JACOBS <jonathanjacobs2113@gmail.com>
2024-09-17 16:54:07 -0400

    Added iced GUI functionality

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,56 @@
 mod text_document;
 
-use std::io;
+use iced::{Element, Sandbox, Settings, Theme, Length, alignment};
+use iced::widget::{container, text, Column, column};
+use crate::text_document::TextDocument;
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    Input,
+    File,
+    Scroll
+}
+
+impl Sandbox for TextDocument {
+    type Message = Message;
+
+    fn new() -> TextDocument {
+        TextDocument {line_buffer: Vec::new(), length: 0}
+    }
+    fn title(&self) -> String {
+        String::from("TextEditor")
+    }
+    fn update(&mut self, message:Message) {
+
+    }
+    fn view(&self) -> Element<'_, Self::Message> {
+        container(
+            paint_text(&self.line_buffer),
+        )
+            .height(Length::Fill)
+            .width(Length::Fill)
+            .into()
+
+    }
+    fn theme(&self) -> iced::Theme {
+        Theme::Dark
+    }
+}
+
+fn paint_text(lines: &Vec<String>) -> Element<'static, Message> {
+    let mut column = Column::new()
+        .spacing(20)
+        .width(Length::Fill);
+    for line in lines {
+        column = column.push(text(line))
+    }
+    container(column)
+        .height(250)
+        .width(300)
+        .into()
+}
+
 
 fn main() {
+    TextDocument::run(Settings::default());
 }
--- src/text_document.rs
+++ src/text_document.rs
@@ -1,15 +1,13 @@
 use std::fs::OpenOptions;
 use std::io::Read;
-struct TextDocument<'a> {
-    init_buffer: bool,
-    line_buffer: Vec<&'a str>,
-    length: usize,
+
+pub struct TextDocument {
+    pub(crate) line_buffer: Vec<String>,
+    pub(crate) length: usize,
 }
 
 impl TextDocument {
-    // Constructor
-    fn init(&mut self, file_name: &str) -> std::io::Result<()>
-    {
+    pub fn init(&mut self, file_name: &str) -> std::io::Result<()> {
         let mut file = OpenOptions::new()
             .read(true)
             .write(true)
@@ -18,15 +16,18 @@ impl TextDocument {
         let mut data: String = String::new();
         file.read_to_string(&mut data)?;
         let lines = data.lines();
-        for line in lines
-        {
-            self.line_buffer.push(line)
+        for line in lines {
+            self.line_buffer.push(line.to_string());
         }
         self.length = self.line_buffer.len();
         Ok(())
     }
 
-    fn get_line<'a>(&self, line_number: usize) -> &'a str {
-        self.line_buffer[line_number]
+    pub fn get_line(&self, line_number: usize) -> String {
+        self.line_buffer.get(line_number).unwrap().to_string()
+    }
+
+    pub fn line_count(&self) -> usize {
+        self.length
     }
 }
